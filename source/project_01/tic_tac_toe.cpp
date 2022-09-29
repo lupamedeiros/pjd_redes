a99af8527ca7e7d70aa9efe128c64f97263d0dc0
Luiz Paulo <luizpaulo13@gmail.com>
2022-09-28 22:31:38 -0300

    renamed programa_01.cpp to tic_tac_toe.cpp

--- source/project_01/tic_tac_toe.cpp
+++ source/project_01/tic_tac_toe.cpp
@@ -1,16 +1,14 @@
 /*******************************************************************************
 Autor: Luiz Paulo de Souza Medeiros
 Disciplina: Programação de Jogos em Rede
-Aula 01 - Programa 01 - Trabalhando com Tempo.
+Aula 01 - Programa 01 - Implementando o Jogo da Velha
 *******************************************************************************/
 
-#include <ctime>
-#include <iostream>
+#include <iostream> // biblioteca padrão de entrada e saída
+
+using namespace std;
 
 int main(){
-    std::time_t result = std::time(nullptr);
-    std::cout << std::asctime(std::localtime(&result))
-              << result << " seconds since the Epoch\n";
 
     return 0;
 }
