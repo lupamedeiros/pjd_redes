33d9a1889414e0a9854b75bd3d7c2033b9a34866
Luiz Paulo de Souza Medeiros <luiz.medeiros@ifrn.edu.br>
2022-10-11 13:48:44 -0300

    Versão Off-Line Concluída.

--- source/project_01/tic_tac_toe.cpp
+++ source/project_01/tic_tac_toe.cpp
@@ -58,29 +58,72 @@ int main(){
     bool jogo_continua = true;
     int posicao_jogada = 0;
     char jogador_ativo = 'O';
+    int contador_jogadas = 0;
     
     while (jogo_continua){
         // Imprimir Tabuleiro
         imprimir_tabuleiro(tabuleiro);
 
         // Receber a posição de jogada;
-        cout << "Indique a posição que quer jogar (1-9): ";
-        cin >> posicao_jogada;
+        bool jogada_valida = false;
+
+        while (!jogada_valida){
+            cout << ((jogador_ativo == 'O') ? jogador_1 : jogador_2)
+                 << ", indique a posição em que quer jogar (1-9): ";
+
+            cin >> posicao_jogada;
+
+            if (posicao_jogada < 1 || posicao_jogada > 9) // Verifica se valor digitado está entre 1 e 9
+                cout << "Posição inválida!" << endl;
+            else if (tabuleiro[posicao_jogada -1] != '-') // Verifica se a posição está disponível
+                cout << "Posição não disponível!" << endl;
+            else
+                jogada_valida = true;
+        }
         
+        // Enviar posição para o tabuleiro
         tabuleiro[posicao_jogada -1] = jogador_ativo;
+        contador_jogadas++;
         
+        // Verificar Condições de Parada
+        // Vitória
+        if ((tabuleiro[0] == jogador_ativo && tabuleiro[1] == jogador_ativo && tabuleiro[2] == jogador_ativo) || // Lin 1
+            (tabuleiro[3] == jogador_ativo && tabuleiro[4] == jogador_ativo && tabuleiro[5] == jogador_ativo) || // Lin 2
+            (tabuleiro[6] == jogador_ativo && tabuleiro[7] == jogador_ativo && tabuleiro[8] == jogador_ativo) || // Lin 3
+            (tabuleiro[0] == jogador_ativo && tabuleiro[3] == jogador_ativo && tabuleiro[6] == jogador_ativo) || // Col 1
+            (tabuleiro[1] == jogador_ativo && tabuleiro[4] == jogador_ativo && tabuleiro[7] == jogador_ativo) || // Col 2
+            (tabuleiro[2] == jogador_ativo && tabuleiro[5] == jogador_ativo && tabuleiro[8] == jogador_ativo) || // Col 3
+            (tabuleiro[0] == jogador_ativo && tabuleiro[4] == jogador_ativo && tabuleiro[8] == jogador_ativo) || // Diag Principal
+            (tabuleiro[2] == jogador_ativo && tabuleiro[4] == jogador_ativo && tabuleiro[6] == jogador_ativo)) { // Diag Inversa
+
+            cout << " ******************************** " << endl
+                 << " " << ((jogador_ativo == 'O') ? jogador_1 : jogador_2)
+                 << " VENCEU!! =D" << endl
+                 << " ******************************** " << endl;
+            
+            imprimir_tabuleiro(tabuleiro);
+
+            jogo_continua = false;
+            break;
+        }
+        // Verificar quantidade de jogadas realizadas
+        else if (contador_jogadas == 9) {
+            cout << " ******************************** " << endl
+                 << " Empate. =/" << endl
+                 << " ******************************** " << endl;
+
+            imprimir_tabuleiro(tabuleiro);
+
+            jogo_continua = false;
+            break;
+        }
+
         if (jogador_ativo == 'O')
             jogador_ativo = 'X';
         else
             jogador_ativo = 'O';
-
-        // Identificar condição de parada;
-        jogo_continua = true; // Deverá receber false se o jogo encerrar;
-    }
     
-    /* FIM DO JOGO
-     - Exibir o resultado.
-    */
+    }
 
     return 0;
 }
