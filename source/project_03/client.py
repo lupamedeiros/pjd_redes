69f873e356babb4c17ab1383a71a1ce13a007495
Luiz Paulo de Souza Medeiros <luiz.medeiros@ifrn.edu.br>
2022-11-25 15:37:35 -0300

    client commented

--- source/project_03/client.py
+++ source/project_03/client.py
@@ -8,10 +8,11 @@ def imprimir_tabuleiro(tabuleiro):
 
 def Main():
 
-    # Define ip do servidor e porta
+    # Define IP do Servidor e Porta
     server = "127.0.0.1"
     port = 8080
 
+    # Leio nome do Jogador do Teclado
     nome = input("Digite seu nome: ")
 
     # Cria o socket e conecta ao servidor
@@ -21,41 +22,49 @@ def Main():
     print("Conectado ao servidor")
     i_buffer = ""
 
+    # Envio o Nome do Jogador
     s.send(nome.encode('utf8'))
 
+    # Recebo a peça que irei jogar
     i_buffer = str(s.recv(1024), encoding='utf8')
     print("Você jogará com " + i_buffer)
 
-    # Definções do Jogo
+    # Definções de Variaveis Jogo
     peca = i_buffer
     tabuleiro = "---------"
 
     # Loop para enviar e receber mensagens
     while True:
+        # Recebo estado atual + tabuleiro
         i_buffer = str(s.recv(1024), encoding='utf8')
         
+        # Verifico se houve vitoria
         if i_buffer[0] == "V":
             tabuleiro = i_buffer[2:]
+            imprimir_tabuleiro(tabuleiro)
             if i_buffer[1] == peca:
-                imprimir_tabuleiro(tabuleiro)
                 print("Você venceu!\n")
             else:
-                imprimir_tabuleiro(tabuleiro)
-                print("Você perdeu!\n")
+                print("Você perdeu... =( \n")
             break
 
+        # Copio tabuleiro para outra variavel e imprimo na tela
         tabuleiro = i_buffer[1:]
         imprimir_tabuleiro(tabuleiro)
 
+        # Verifico se houve empate
         if i_buffer[0] == "E":
             print("Empate!\n")
             break
 
+        # Verifico se eh minha vez
         if i_buffer[0] == peca:
+            # Leio a jogada e envio para o servidor
             print("É sua vez de jogar")
             o_buffer = input("Digite a posição que deseja jogar: ")
             s.send(o_buffer.encode('utf8'))
         else:
+            # Informo que estou aguardando o outro jogador
             print("Aguardando o outro jogador")
 
 if __name__=="__main__":
